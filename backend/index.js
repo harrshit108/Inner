f12c26d2c6b929303091e3727a2a70aaa89b8584
Harshit Yadav <harshit.yadav108@gmail.com>
2025-02-17 20:16:07 +0530

    crud-from-the-dashboard

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const { HoldingsModel } = require('./model/HoldingsModel')
 const { PositionsModel } = require('./model/PositionsModel');
+const { OrdersModel } = require('./model/OrdersModel');
 
 const PORT = process.env.PORT || 8080;
 const uri = process.env.MONGO_URL ;
@@ -184,7 +185,18 @@ app.use(bodyParser.json());
 //       res.send("Done!");
 // });
 
+app.post("/newOrder", async(req, res) => {
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty : req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
 
+    newOrder.save();
+
+    res.send("Order saved");
+});
 
 app.get('/allHoldings', async(req, res) =>{
     let allHoldings = await HoldingsModel.find({});
