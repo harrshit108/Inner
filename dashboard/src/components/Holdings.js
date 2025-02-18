d4992a22671116e07054fd5487830991e1680895
Harshit Yadav <harshit.yadav108@gmail.com>
2025-02-18 16:15:35 +0530

    plotting-graphs-with-chartJS

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect} from "react";
 
 import  axios  from 'axios';
 
+import { VerticalGraph } from "./VerticalGraph";
+
 // import { holdings } from "../data/data"
 
 const Holdings = () => {
@@ -14,6 +16,19 @@ const Holdings = () => {
       setAllHoldings(res.data);
     });
   }, []);
+
+const labels  = allHoldings.map((subArray) => subArray["name"]);
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Stock Price',
+      data: allHoldings.map((stock) => stock.price),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};  
 
   return (
     <>
@@ -73,6 +88,7 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   );
 };
